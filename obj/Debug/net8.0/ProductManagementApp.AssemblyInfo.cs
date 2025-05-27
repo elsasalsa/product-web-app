ec95ad729e99e2aaeb417afe0ba31e4893c4c8ff
Elsa8989 <elsasalsabilla@smkwikrama.sch.id>
2025-05-28 06:03:59 +0700

    prodify

--- obj/Debug/net8.0/ProductManagementApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2731816e2ad5598feda7ac4f289e0e248d49bca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111db454f27425fc305b96ba4116fffb967b4774")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
